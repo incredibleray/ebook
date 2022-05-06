5b7f2b090c648918535f08589fefcb99b964f611
Ray Xu <ruijxu@microsoft.com>
2022-05-06 11:59:37 -0700

    c

--- public/electron.js
+++ public/electron.js
@@ -2,12 +2,14 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    show: false
+    // fullscreen: true
   })
-
+  win.maximize();
   win.loadFile('build/index.html');
   // win.loadURL('http://localhost:3000');
+
+  win.show();
 }
 
 app.whenReady().then(() => {
